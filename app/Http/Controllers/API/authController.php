370183b648707b8c8bbbcc5f710967317f55b2e3
mdSoroarJahan <soroarjahan17@gmail.com>
2026-04-16 01:06:13 +0600

    Implement user registration and login request validation

--- app/Http/Controllers/API/authController.php
+++ app/Http/Controllers/API/authController.php
@@ -4,9 +4,20 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\Base\BaseApiController;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class authController extends BaseApiController
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        return $this->success(new UserResource($user), 'User registered successfully', 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\authController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    //Auth routes
+    Route::post('/register', [authController::class, 'register']);
+});
